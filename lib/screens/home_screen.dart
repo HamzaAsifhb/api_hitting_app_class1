44b55beea666d5a0d2a3c32de4f2802de57b082e
HamzaAsifHb <hamzaasifhb@gmail.com>
2023-12-06 04:18:32 +0500

    Called Simple Api example for studetns

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  Future<List<Map<String, dynamic>>> fetchposts() async {
+  final response =
+      await http.get(Uri.parse("https://jsonplaceholder.typicode.com/posts"));
+  if (response.statusCode == 200) {
+    List<dynamic> rawData = json.decode(response.body);
+    List<Map<String, dynamic>> data = rawData.cast<Map<String, dynamic>>();
+    return data;
+  } else {
+    throw Exception("failed to load data");
+  }
+}
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Api sy khud laarhy hain bhai"),
+      ),
+      body: FutureBuilder(
+        future: fetchposts(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          } else if (snapshot.hasError) {
+            return Center(
+              child: Text("Error: ${snapshot.error}"),
+            );
+          } else if (snapshot.hasData && snapshot.data != null) {
+            return ListView.builder(
+              itemCount: snapshot.data!.length,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  title: Text(snapshot.data![index]["title"]),
+                  subtitle: Text(snapshot.data![index]["body"]),
+                );
+              },
+            );
+          } else {
+            return const Center(
+              child: Text("No data available"),
+            );
+          }
+        },
+      ),
+    );
+  }
+}
